18f6b84e4e21556544b36b7aab65f8248aee7c15
DistantParadox <craigtski47@gmail.com>
2017-02-09 13:54:20 +0000

    Completed Chapter 6 exercises

--- rango/views.py
+++ rango/views.py
@@ -13,9 +13,11 @@ def index(request):
     # that will be passed to the template engine
     category_list = Category.objects.order_by('-likes')[:5]
 
+    pages_list = Page.objects.order_by('-views')[:5]
+
     # Construct a dictionary to pass to the template engine as its context
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'categories': category_list}
+    context_dict = {'categories': category_list, 'pages': pages_list}
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make out lives easier
