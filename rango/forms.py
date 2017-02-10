d51f9e0a10c850af6545706bb0d70e3c56b5e209
DistantParadox <craigtski47@gmail.com>
2017-02-10 13:15:38 +0000

    Created forms for user profiles (ch.9).

--- rango/forms.py
+++ rango/forms.py
@@ -45,3 +45,15 @@ class PageForm(forms.ModelForm):
         exclude = ('category',)
         # or specify the fields to include (i.e. not include the category field)
         #fields = ('title', 'url', 'views')
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+	fields = ('username', 'email', 'password')
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+	fields = ('website', 'picture')
--- rango/models.py
+++ rango/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib import admin
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
@@ -39,3 +40,16 @@ class PageAdmin(admin.ModelAdmin):
     def __str__(self): # For Python 2, use __unicode__ too
 
         return self.title
+
+class UserProfile(models.Model):
+    # This line is required. Links UserProfile to a User model instance.
+    user = models.OneToOneField(User)
+
+    # The additional attributes we wish to include
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    # Override the __unicode__() method to return out something meaningful!
+    # Remember if you use python 2.7.x,  define __unicode__ too!
+    def __str__(self):
+        return self.user.username
